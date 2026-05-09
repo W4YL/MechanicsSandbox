fe07d43e0f58358d10842a23eefc4308e7c8a518
W4ckyYL <yanzixi.w4yl@gmail.com>
2026-05-08 21:50:06 -0700

    Pixel filter!

--- Assets/Scripts/BackgroundParallax.cs
+++ Assets/Scripts/BackgroundParallax.cs
@@ -8,24 +8,23 @@ public class BackgroundParallax : MonoBehaviour
     public float cloudSpeed;
 
     float cloudMovement;
-
-    Vector2 newPos;
+    float parallaxMult;
 
     void Start()
     {
-        newPos = transform.position;
+
     }
 
     void Update()
     {
         cloudMovement -= Time.deltaTime * cloudSpeed;
+        parallaxMult = parallaxValue * 0.0625f;
 
         if (cloudMovement <= -77.5f)
         {
             cloudMovement = 0;
         }
 
-        newPos.x = cameraObject.transform.position.x * parallaxValue + cloudMovement;
-        transform.position = newPos;
+        transform.position = new Vector2(cameraObject.transform.position.x * parallaxMult + cloudMovement, transform.position.y);
     }
 }
